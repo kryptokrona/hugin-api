7768546cc3265b95b7639fe73d2184489b170b60
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-05-04 20:40:05 +0200

    Adding more unit tests

--- tests/hashtag.js
+++ tests/hashtag.js
@@ -9,12 +9,12 @@ let chaiHttp = require('chai-http')
 chai.should()
 chai.use(chaiHttp)
 
-describe('Post APIs', () => {
+describe('Hashtag APIs', () => {
 
-    describe(`Test GET route ${process.env.API_BASE_PATH}/posts`, () => {
-        it('It should return all latest posts', (done) => {
+    describe(`Test GET route ${process.env.API_BASE_PATH}/hashtags/trending`, () => {
+        it('It should return all trending hashtags', (done) => {
             chai.request(server)
-                .get(`${process.env.API_BASE_PATH}/posts`)
+                .get(`${process.env.API_BASE_PATH}/hashtags/trending`)
                 .end((err, response) => {
                     response.should.have.status(200)
                     response.body.should.be.a('object')
@@ -24,38 +24,12 @@ describe('Post APIs', () => {
         })
     })
 
-    //TODO: get post by id test here
+    //TODO: add a successful 200 when trying to get a hashtag that exists, after we fix mock data.
 
-    describe(`Test GET route ${process.env.API_BASE_PATH}/posts/latest`, () => {
-        it('It should return all latest posts', (done) => {
+    describe(`Test GET route ${process.env.API_BASE_PATH}/hashtags/1000`, () => {
+        it('It should return a 404 when trying to get a hashtag that does not exist', (done) => {
             chai.request(server)
-                .get(`${process.env.API_BASE_PATH}/posts/latest`)
-                .end((err, response) => {
-                    response.should.have.status(200)
-                    response.body.should.be.a('object')
-                    // response.body.length.should.not.be.eq(0) // uncomment this later when we have actual data to get back from DB
-                })
-            done()
-        })
-    })
-
-    describe(`Test GET route ${process.env.API_BASE_PATH}/posts/latest?size=10&page=0`, () => {
-        it('It should return posts on page 0 with a limit of 10 posts', (done) => {
-            chai.request(server)
-                .get(`${process.env.API_BASE_PATH}/posts/latest?size=10&page=0`)
-                .end((err, response) => {
-                    response.should.have.status(200)
-                    response.body.should.be.a('object')
-                    // response.body.length.should.not.be.eq(0) // uncomment this later when we have actual data to get back from DB
-                })
-            done()
-        })
-    })
-
-    describe(`Test GET route ${process.env.API_BASE_PATH}/posts/08d9a3158ff7111e8a1a8f0c6012039dff1b34fbbdfe3e9a8e5e399452fdba16`, () => {
-        it('It should return a 404 since the tx_hash value does not exist in db', (done) => {
-            chai.request(server)
-                .get(`${process.env.API_BASE_PATH}/posts/08d9a3158ff7111e8a1a8f0c6012039dff1b34fbbdfe3e9a8e5e399452fdba16`)
+                .get(`${process.env.API_BASE_PATH}/hashtags/1000`)
                 .end((err, response) => {
                     response.should.have.status(404)
                     response.body.should.be.a('object')
@@ -65,5 +39,30 @@ describe('Post APIs', () => {
         })
     })
 
-    //TODO: add a 200 for tx_hash test with mock data
+    describe(`Test GET route ${process.env.API_BASE_PATH}/hashtags`, () => {
+        it('It should return all hashtags', (done) => {
+            chai.request(server)
+                .get(`${process.env.API_BASE_PATH}/hashtags`)
+                .end((err, response) => {
+                    response.should.have.status(200)
+                    response.body.should.be.a('object')
+                    // response.body.length.should.not.be.eq(0) // uncomment this later when we have actual data to get back from DB
+                })
+            done()
+        })
+    })
+
+    describe(`Test GET route ${process.env.API_BASE_PATH}/hashtags/latest`, () => {
+        it('It should return all latest hashtags', (done) => {
+            chai.request(server)
+                .get(`${process.env.API_BASE_PATH}/hashtags/latest`)
+                .end((err, response) => {
+                    response.should.have.status(200)
+                    response.body.should.be.a('object')
+                    // response.body.length.should.not.be.eq(0) // uncomment this later when we have actual data to get back from DB
+                })
+            done()
+        })
+    })
+
 })
