4ea29381409a7e9b9368a22d57e741354bc8318d
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-05-07 00:20:39 +0200

    Updating load testing

--- tests/k6/loadTest.js
+++ tests/k6/loadTest.js
@@ -1,16 +1,16 @@
-let http = require('k6/http') 
-let sleep = require('k6')
+import http from 'k6/http'
+import { sleep } from 'k6'
 
-module.exports.options = {
+export const options = {
   duration: '1m',
   vus: 50,
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
   },
-};
+}
 
-module.exports = () => {
-  const res = http.get('http://hugin-cache:3000') // we use the docker-compose service name here
+export default function () {
+  const res = http.get('http://hugin-cache:3000')
   sleep(1)
 }
