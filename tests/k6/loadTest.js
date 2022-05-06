d7662bd00b1f4433cb80b92a98261be6552501a0
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-05-06 23:47:00 +0200

    Adding k6 load testing pipeline

---
+++ tests/k6/loadTest.js
@@ -0,0 +1,16 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
+};
+
+export default function () {
+  const res = http.get('http://hugin-cache:3000') // we use the docker-compose service name here
+  sleep(1)
+}
