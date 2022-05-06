5e6ba6726dd073823adf2c27e3b55cb736196f64
Marcus Cvjeticanin <33717111+mjovanc@users.noreply.github.com>
2022-05-07 00:23:59 +0200

    Feature/load testing (#43)
    * Adding k6 load testing pipeline
    
    * Adding k6 load testing pipeline
    
    * Updating load testing

--- tests/k6/loadTest.js
+++ tests/k6/loadTest.js
@@ -8,9 +8,9 @@ module.exports.options = {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
   },
-};
+}
 
-module.exports = () => {
-  const res = http.get('http://hugin-cache:3000') // we use the docker-compose service name here
+export default function () {
+  const res = http.get('http://hugin-cache:3000')
   sleep(1)
 }
