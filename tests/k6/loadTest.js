9aa1978b9604f3c5b5b5fe4e5924bd85b4c692a3
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-05-07 12:29:55 +0200

    Updating loadTest.js

--- tests/k6/loadTest.js
+++ tests/k6/loadTest.js
@@ -1,25 +1,26 @@
 import http from 'k6/http'
-import { check, group, sleep } from 'k6'
-
-export const options = {
-  stages: [
-    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
-  ],
-  thresholds: {
-    'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
-  },
+import { check, group } from 'k6'
+ 
+export let options = {
+   stages: [
+       { duration: '0.5m', target: 3 }, // simulate ramp-up of traffic from 1 to 3 virtual users over 0.5 minutes.
+       { duration: '0.5m', target: 4}, // stay at 4 virtual users for 0.5 minutes
+       { duration: '0.5m', target: 0 }, // ramp-down to 0 users
+     ],
 }
 
 const BASE_URL = 'http://localhost:3000';
+ 
+export default function () {
+   group('API uptime check', () => {
+       const responsePosts = http.get(`${BASE_URL}/api/v1/posts`)
+       check(responsePosts, {
+           "status code should be 200": res => res.status === 200,
+       })
 
-export default () => {
-  const allPosts = http.get(`${BASE_URL}/api/v1/posts`).json()
-  check(allPosts, { 'retrieved posts': (obj) => obj.length > 0 })
-
-  const allHashtags = http.get(`${BASE_URL}/api/v1/hashtags`).json()
-  check(allHashtags, { 'retrieved hashtags': (obj) => obj.length > 0 })
-
-  sleep(1)
+       const responseHashtags = http.get(`${BASE_URL}/api/v1/hashtags`)
+       check(responseHashtags, {
+           "status code should be 200": res => res.status === 200,
+       })
+   })
 }
