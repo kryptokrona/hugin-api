ae6998d882c2ee53782f16714461dd500798180f
Marcus Cvjeticanin <33717111+mjovanc@users.noreply.github.com>
2022-05-08 17:18:29 +0200

    Feature/configurable system (#47)
    * Updating setup.sh deploy script
    
    * Updating Ansible

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
+# this file is intended for manually deploy from your local computer and not in the GitHub Actions workflow
 
-ansible-playbook provision_vps.yml -i hosts.inventory --vault-password-file=~/hugin.cache.vault
+# checks if keypair doesn't exist
+if [ ! -f ~/.ssh/id_hugin_cache ]; then
+    ssh-keygen -t rsa -N "" -f ~/.ssh/id_hugin_cache
+    eval "$(ssh-agent -s)" &>/dev/null
+    ssh-add ~/.ssh/id_hugin_cache &>/dev/null
+    ssh-copy-id -i ~/.ssh/id_hugin_cache.pub root@cache.hugin.chat
+fi
+
+ansible-playbook provision_vps.yml -i hosts.inventory --ask-vault-pass
--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,24 +2,35 @@
 # 
 # This file is used for setting up environment variables and installing dependencies
 
-# Edit these variables with what you prefer
-POSTGRES_DB=hugin_cache_prod
-POSTGRES_USER=postgres
-POSTGRES_PASSWORD=test1234
-DATABASE_URL=postgres://postgres:$POSTGRES_PASSWORD@127.0.0.1:5432/$POSTGRES_DB
-DOMAINS=(example.org www.example.org)
-EMAIL=user@user.com
+# General configs
+SYS_HUGIN_NODE_ENV=production # do not change this
+SYS_HUGIN_NODE_SERVER=blocksum.org:11898
+SYS_HUGIN_DOCKER_IMAGE=ghcr.io/kryptokrona/hugin-cache:latest
+SYS_HUGIN_PORT=3000
 
-# System configs
-#TODO: add variables here and below and these will be used with docker-compose -e FOO=BAR and then execute cron job to clean etc
+# NGINX
+SYS_NGINX_DOMAINS=(example.org www.example.org)
+SYS_NGINX_EMAIL=user@user.com
+
+# Database configs
+SYS_POSTGRES_DB=hugin_cache_prod
+SYS_POSTGRES_USER=postgres
+SYS_POSTGRES_PASSWORD=test1234
+SYS_POSTGRES_PORT=5432
+SYS_POSTGRES_URL=postgres://$SYS_POSTGRES_USER:$SYS_POSTGRES_PASSWORD@127.0.0.1:$SYS_POSTGRES_PORT/$SYS_POSTGRES_DB
 
 # Setting environment variables as persistent
-echo "export POSTGRES_DB=$POSTGRES_DB" >> ~/.bashrc
-echo "export POSTGRES_USER=$POSTGRES_USER" >> ~/.bashrc
-echo "export POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> ~/.bashrc
-echo "export DATABASE_URL=$DATABASE_URL" >> ~/.bashrc
-echo "export DOMAINS=$DOMAINS" >> ~/.bashrc
-echo "export EMAIL=$EMAIL" >> ~/.bashrc
+echo "export SYS_HUGIN_NODE_ENV=$SYS_HUGIN_NODE_ENV" >> ~/.bashrc
+echo "export SYS_HUGIN_NODE_SERVER=$SYS_HUGIN_NODE_SERVER" >> ~/.bashrc
+echo "export SYS_HUGIN_DOCKER_IMAGE=$SYS_HUGIN_DOCKER_IMAGE" >> ~/.bashrc
+echo "export SYS_HUGIN_PORT=$SYS_HUGIN_PORT" >> ~/.bashrc
+echo "export SYS_NGINX_DOMAINS=$SYS_NGINX_DOMAINS" >> ~/.bashrc
+echo "export SYS_NGINX_EMAIL=$SYS_NGINX_EMAIL" >> ~/.bashrc
+echo "export SYS_POSTGRES_DB=$SYS_POSTGRES_DB" >> ~/.bashrc
+echo "export SYS_POSTGRES_USER=$SYS_POSTGRES_USER" >> ~/.bashrc
+echo "export SYS_POSTGRES_PASSWORD=$SYS_POSTGRES_PASSWORD" >> ~/.bashrc
+echo "export SYS_POSTGRES_PORT=$SYS_POSTGRES_PORT" >> ~/.bashrc
+echo "export SYS_POSTGRES_URL=$SYS_POSTGRES_URL" >> ~/.bashrc
 source ~/.bash_rc
 
 # Update
