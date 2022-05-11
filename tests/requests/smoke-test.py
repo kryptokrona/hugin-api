0fcb036d7c3dba2353da9f3cfc1128c774f42299
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-05-11 22:44:07 +0200

    Updating Smoke Test

--- tests/requests/smoke-test.py
+++ tests/requests/smoke-test.py
@@ -3,22 +3,27 @@ import sys
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 try:
-    base_url = 'https://cache.hugin.chat/api/v1/{0}'
+    base_url = 'https://cache.hugin.chat/api/v1/{}'
 
     # trying posts endpoints
-    posts = requests.get(base_url.format('/posts'))
-    assert(posts.status_code == requests.codes.ok)
+    print('GET ' + base_url.format('posts'))
+    posts = requests.get(base_url.format('posts'))
+    assert posts.status_code == requests.codes.ok
 
-    posts = requests.get(base_url.format('/latest'))
-    assert(posts.status_code == requests.codes.ok)
+    print('GET ' + base_url.format('posts/latest'))
+    posts = requests.get(base_url.format('posts/latest'))
+    assert posts.status_code == requests.codes.ok
 
-    # trying hashtag endpoints
-    posts = requests.get(base_url.format('/hashtags'))
-    assert(posts.status_code == requests.codes.ok)
+    # # trying hashtag endpoints
+    print('GET ' + base_url.format('hashtags'))
+    posts = requests.get(base_url.format('hashtags'))
+    assert posts.status_code == requests.codes.ok
 
-    posts = requests.get(base_url.format('/latest'))
-    assert(posts.status_code == requests.codes.ok)
+    print('GET ' + base_url.format('hashtags/latest'))
+    posts = requests.get(base_url.format('hashtags/latest'))
+    assert posts.status_code == requests.codes.ok
 
+    print('\nSMOKE TEST PASSED!')
 except (AssertionError, ConnectionError, Timeout, TooManyRedirects):
     print('Smoke Test Failed!')
     sys.exit(1)
