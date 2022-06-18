e65c999c4e5c3dcbd3c02a3a5484acee804d235a
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-06-18 15:37:44 +0200

    Adding profanity-list.json with curse words

--- utils/messageCriteria.js
+++ utils/messageCriteria.js
@@ -5,6 +5,7 @@
 'use strict'
 
 require('dotenv').config()
+const profanityList = require('../profanity-list.json')
 let log = require('loglevel')
 
 /**
@@ -13,7 +14,22 @@ let log = require('loglevel')
  * @param {object} message - Message object.
  * @returns {Boolean} criteria - True or false based on the criteria of the config.
  */
-module.exports.messageCriteria = (message) => {
-    
-    return false
+module.exports.messageCriteria = (messageObj) => {
+    usersInclude = process.env.SYS_CRITERIA_USERS_INCLUDE
+    usersExclude = process.env.SYS_CRITERIA_USERS_EXCLUDE
+
+    boardsInclude = process.env.SYS_CRITERIA_BOARDS_INCLUDE
+    boardsExclude = process.env.SYS_CRITERIA_BOARDS_EXCLUDE
+
+    keywordsInclude = process.env.SYS_CRITERIA_KEYWORDS_INCLUDE
+    keywordsExclude = process.env.SYS_CRITERIA_KEYWORDS_EXCLUDE
+    keywordsCursewords = process.env.SYS_CRITERIA_KEYWORDS_CURSEWORDS
+
+    // check for curse words
+    if (!keywordsCursewords) {
+        found = profanityList.find(word => messageObj.message === word)
+        if (found) return false
+    }
+
+    return true
 }
