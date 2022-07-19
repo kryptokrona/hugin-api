1423956e94773d2a5753012ec3015206984d06f2
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-07-19 22:41:06 +0200

    Adding new tests to smoke test

--- tests/requests/smoke-test.py
+++ tests/requests/smoke-test.py
@@ -3,25 +3,35 @@ import sys
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 try:
-    base_url = 'https://cache.hugin.chat/api/v1/{}'
+    base_url = 'https://cache.hugin.chat/{}'
 
     # trying posts endpoints
-    print('GET ' + base_url.format('posts'))
-    posts = requests.get(base_url.format('posts'))
+    print('GET ' + base_url.format('api/v1/posts'))
+    posts = requests.get(base_url.format('api/v1/posts'))
     assert posts.status_code == requests.codes.ok
 
-    print('GET ' + base_url.format('posts/latest'))
-    posts = requests.get(base_url.format('posts/latest'))
+    print('GET ' + base_url.format('api/v1/posts/latest'))
+    posts = requests.get(base_url.format('api/v1/posts/latest'))
     assert posts.status_code == requests.codes.ok
 
-    # # trying hashtag endpoints
-    print('GET ' + base_url.format('hashtags'))
-    posts = requests.get(base_url.format('hashtags'))
+    # trying hashtag endpoints
+    print('GET ' + base_url.format('api/v1/hashtags'))
+    posts = requests.get(base_url.format('api/v1/hashtags'))
     assert posts.status_code == requests.codes.ok
 
-    print('GET ' + base_url.format('hashtags/latest'))
-    posts = requests.get(base_url.format('hashtags/latest'))
+    print('GET ' + base_url.format('api/v1/hashtags/latest'))
+    posts = requests.get(base_url.format('api/v1/hashtags/latest'))
     assert posts.status_code == requests.codes.ok
+
+    # trying prometheus metrics
+    print('GET ' + base_url.format('metrics'))
+    prometheus_metrics = requests.get(base_url.format('metrics'))
+    assert prometheus_metrics.status_code == requests.codes.ok
+
+    # trying hugin cache dashboard
+    print('GET ' + base_url.format('dashboard'))
+    dashboard = requests.get(base_url.format('dashboard'))
+    assert dashboard.status_code == requests.codes.ok
 
     print('\nSMOKE TEST PASSED!')
 except (AssertionError, ConnectionError, Timeout, TooManyRedirects):
