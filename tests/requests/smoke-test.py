e913720e3c3b9f47fd683bdcc4a7e98089af155e
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-07-23 15:33:45 +0200

    Updating smoke test

--- tests/requests/smoke-test.py
+++ tests/requests/smoke-test.py
@@ -3,34 +3,35 @@ import sys
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 try:
-    base_url = 'https://cache.hugin.chat/{}'
+    domain = str(sys.argv[1])
+    base_url = 'https://{}/{}'
 
     # trying posts endpoints
-    print('GET ' + base_url.format('api/v1/posts'))
-    posts = requests.get(base_url.format('api/v1/posts'))
+    print('GET ' + base_url.format(domain ,'api/v1/posts'))
+    posts = requests.get(base_url.format(domain, 'api/v1/posts'))
     assert posts.status_code == requests.codes.ok
 
-    print('GET ' + base_url.format('api/v1/posts/latest'))
-    posts = requests.get(base_url.format('api/v1/posts/latest'))
+    print('GET ' + base_url.format(domain, 'api/v1/posts/latest'))
+    posts = requests.get(base_url.format(domain, 'api/v1/posts/latest'))
     assert posts.status_code == requests.codes.ok
 
     # trying hashtag endpoints
-    print('GET ' + base_url.format('api/v1/hashtags'))
-    posts = requests.get(base_url.format('api/v1/hashtags'))
+    print('GET ' + base_url.format(domain, 'api/v1/hashtags'))
+    posts = requests.get(base_url.format(domain, 'api/v1/hashtags'))
     assert posts.status_code == requests.codes.ok
 
-    print('GET ' + base_url.format('api/v1/hashtags/latest'))
-    posts = requests.get(base_url.format('api/v1/hashtags/latest'))
+    print('GET ' + base_url.format(domain, 'api/v1/hashtags/latest'))
+    posts = requests.get(base_url.format(domain, 'api/v1/hashtags/latest'))
     assert posts.status_code == requests.codes.ok
 
     # trying prometheus metrics
-    print('GET ' + base_url.format('metrics'))
-    prometheus_metrics = requests.get(base_url.format('metrics'))
+    print('GET ' + base_url.format(domain, 'metrics'))
+    prometheus_metrics = requests.get(base_url.format(domain, 'metrics'))
     assert prometheus_metrics.status_code == requests.codes.ok
 
     # trying hugin cache dashboard
-    print('GET ' + base_url.format('dashboard'))
-    dashboard = requests.get(base_url.format('dashboard'))
+    print('GET ' + base_url.format(domain, 'dashboard'))
+    dashboard = requests.get(base_url.format(domain, 'dashboard'))
     assert dashboard.status_code == requests.codes.ok
 
     print('\nSMOKE TEST PASSED!')
