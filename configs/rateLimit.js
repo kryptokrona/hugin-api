7fb4cd1997d62c0713abe8de9fc56128fd1b0ff9
Coffeboi <84473858+Coffeboi@users.noreply.github.com>
2022-08-22 16:36:53 +0200

    183 make rate limit setting (#186)
    * Added two lines to .env
    
    * change the values to use an environment variable:
    
    * Added ratelimit to provision_vps.yml

--- configs/rateLimit.js
+++ configs/rateLimit.js
@@ -8,8 +8,8 @@ require('dotenv').config()
 const rateLimit = require("express-rate-limit")
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 1000, // Limit each IP to 1000 requests per `window` (here, per 15 minutes) - TODO: perhaps put this in a settings.ini file?
+  windowMs: process.env.SYS_RATELIMIT_WINDOW_MS, // 15 minutes
+  max: process.env.SYS_RATELIMIT_MAX, // Limit each IP to 1000 requests per `window` (here, per 15 minutes) - TODO: perhaps put this in a settings.ini file?
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
