6718ac771b96075f4f41f28f79064dcca2a3df0d
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 01:00:39 +0200

    Optimizing imports

--- src/main/java/org/kryptokrona/hugin/repository/PostRepository.java
+++ src/main/java/org/kryptokrona/hugin/repository/PostRepository.java
@@ -1,8 +1,6 @@
 package org.kryptokrona.hugin.repository;
 
 import org.kryptokrona.hugin.model.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
