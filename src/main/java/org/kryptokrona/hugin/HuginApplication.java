da270a0495ac9572d047c62f416fba3a27478b80
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-14 23:46:39 +0200

    Changing to Spring project

---
+++ src/main/java/org/kryptokrona/hugin/HuginApplication.java
@@ -0,0 +1,13 @@
+package org.kryptokrona.hugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuginApplication.class, args);
+	}
+
+}
