f03db4be6517df368507ca923fc7376cffee34e3
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-16 00:48:21 +0200

    Adding HuginCrypto and ExtraData class

---
+++ src/main/java/org/kryptokrona/hugin/crypto/ExtraData.java
@@ -0,0 +1,18 @@
+package org.kryptokrona.hugin.crypto;
+
+/**
+ * Extra Data.
+ *
+ * @author Marcus Cvjeticanin
+ */
+public class ExtraData {
+
+	private String key;
+
+	private String from;
+
+	private String message;
+
+	private long timestamp;
+
+}
