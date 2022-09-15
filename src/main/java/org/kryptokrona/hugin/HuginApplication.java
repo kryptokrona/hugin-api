d48847a933fd91f1c73f01865134aa663e0eaf5d
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 16:08:35 +0200

    Implementing HuginSyncer

--- src/main/java/org/kryptokrona/hugin/HuginApplication.java
+++ src/main/java/org/kryptokrona/hugin/HuginApplication.java
@@ -2,10 +2,12 @@ package org.kryptokrona.hugin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableScheduling
 public class HuginApplication {
 
 	public static void main(String[] args) {
