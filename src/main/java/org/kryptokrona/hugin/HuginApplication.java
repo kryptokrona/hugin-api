3fd8a585c6eafd4f2d075506e82a22cc7a295c4e
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 15:12:06 +0200

    Adding JavaDoc and updating dependencies

--- src/main/java/org/kryptokrona/hugin/HuginApplication.java
+++ src/main/java/org/kryptokrona/hugin/HuginApplication.java
@@ -2,7 +2,9 @@ package org.kryptokrona.hugin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HuginApplication {
 
