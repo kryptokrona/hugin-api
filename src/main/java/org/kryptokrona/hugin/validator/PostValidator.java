d3366929b7f02325214e2539a480768207ecb8ff
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 15:35:10 +0200

    Adding JavaDoc to PostValidator

--- src/main/java/org/kryptokrona/hugin/validator/PostValidator.java
+++ src/main/java/org/kryptokrona/hugin/validator/PostValidator.java
@@ -9,6 +9,12 @@ import org.kryptokrona.hugin.http.Post;
  */
 public class PostValidator {
 
+	/**
+	 * Validates if a post object has correct attributes.
+	 *
+	 * @param post - Post HTTP object
+	 * @return Returns true or false
+	 */
 	public static boolean validatePost(Post post) {
 		var isKeyValidated = validateKey(post.getKey());
 		var isSignatureValidated = validateSignature(post.getSignature());
@@ -19,18 +25,42 @@ public class PostValidator {
 		return isKeyValidated && isSignatureValidated && isTimeValidated && isTxHashValidated && isReplyTxHashValidated;
 	}
 
+	/**
+	 * Validates if a given key is of correct format.
+	 *
+	 * @param key - The Post HTTP object key
+	 * @return Returns true or false
+	 */
 	public static boolean validateKey(String key) {
 		return false;
 	}
 
+	/**
+	 * Validates if a given signature is of correct format.
+	 *
+	 * @param signature - The Post HTTP object signature
+	 * @return Returns true or false
+	 */
 	public static boolean validateSignature(String signature) {
 		return false;
 	}
 
+	/**
+	 * Validates if a given time is of correct format.
+	 *
+	 * @param time - The Post HTTP object time
+	 * @return Returns true or false
+	 */
 	public static boolean validateTime(long time) {
 		return false;
 	}
 
+	/**
+	 * Validates if a given transaction hash is of correct format.
+	 *
+	 * @param txHash - The Post HTTP object transaction hash
+	 * @return Returns true or false
+	 */
 	public static boolean validateTxHash(String txHash) {
 		return false;
 	}
