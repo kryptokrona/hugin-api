9eab6b07567fdee274f70e5a1792c2691b93ed10
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 21:33:56 +0200

    Adding more JavaDoc

--- src/main/java/org/kryptokrona/hugin/http/VoutTarget.java
+++ src/main/java/org/kryptokrona/hugin/http/VoutTarget.java
@@ -3,6 +3,8 @@ package org.kryptokrona.hugin.http;
 /**
  * Vout Target HTTP model.
  *
+ * Response object after a POST request to /get_pool_changes_lite to a node.
+ *
  * @author Marcus Cvjeticanin
  */
 public class VoutTarget {
