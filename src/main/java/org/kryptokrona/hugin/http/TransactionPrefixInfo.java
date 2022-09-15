86de7be3bd567c09af9c8726aacdaf9a87b6c7b7
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 21:33:56 +0200

    Adding more JavaDoc

--- src/main/java/org/kryptokrona/hugin/http/TransactionPrefixInfo.java
+++ src/main/java/org/kryptokrona/hugin/http/TransactionPrefixInfo.java
@@ -3,6 +3,8 @@ package org.kryptokrona.hugin.http;
 /**
  * Transaction Prefix Info HTTP model.
  *
+ * Response object after a POST request to /get_pool_changes_lite to a node.
+ *
  * @author Marcus Cvjeticanin
  */
 public class TransactionPrefixInfo {
