500ebb9f356fa2e46b5fd60e8b66b261b837c224
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 17:33:29 +0200

    Updating models

---
+++ src/main/java/org/kryptokrona/hugin/model/http/Post.java
@@ -0,0 +1,89 @@
+package org.kryptokrona.hugin.model.http;
+
+/**
+ * Post HTTP model that represent data we get from the node.
+ *
+ * @author Marcus Cvjeticanin
+ */
+public class Post {
+
+	private String message;
+
+	private String key;
+
+	private String signature;
+
+	private String board;
+
+	private long time;
+
+	private String nickname;
+
+	private String txHash;
+
+	private String replyTxHash;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+
+	public String getBoard() {
+		return board;
+	}
+
+	public void setBoard(String board) {
+		this.board = board;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getTxHash() {
+		return txHash;
+	}
+
+	public void setTxHash(String txHash) {
+		this.txHash = txHash;
+	}
+
+	public String getReplyTxHash() {
+		return replyTxHash;
+	}
+
+	public void setReplyTxHash(String replyTxHash) {
+		this.replyTxHash = replyTxHash;
+	}
+}
