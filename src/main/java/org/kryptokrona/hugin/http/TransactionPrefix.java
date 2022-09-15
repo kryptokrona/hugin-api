fbce18623a29464a8170b1bb2e0cb523229057aa
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-15 21:46:54 +0200

    Adding new Type in HuginSyncer

--- src/main/java/org/kryptokrona/hugin/http/TransactionPrefix.java
+++ src/main/java/org/kryptokrona/hugin/http/TransactionPrefix.java
@@ -21,4 +21,43 @@ public class TransactionPrefix {
 
 	private List<Vout> voutList;
 
+	public String getExtra() {
+		return extra;
+	}
+
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+
+	public long getUnlockTime() {
+		return unlockTime;
+	}
+
+	public void setUnlockTime(long unlockTime) {
+		this.unlockTime = unlockTime;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public List<Vin> getVinList() {
+		return vinList;
+	}
+
+	public void setVinList(List<Vin> vinList) {
+		this.vinList = vinList;
+	}
+
+	public List<Vout> getVoutList() {
+		return voutList;
+	}
+
+	public void setVoutList(List<Vout> voutList) {
+		this.voutList = voutList;
+	}
 }
--- src/main/java/org/kryptokrona/hugin/http/VoutTargetData.java
+++ src/main/java/org/kryptokrona/hugin/http/VoutTargetData.java
@@ -11,4 +11,11 @@ public class VoutTargetData {
 
 	private String key;
 
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
 }
