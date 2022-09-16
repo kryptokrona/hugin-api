16cca85249a2245e0ee5963ea7102232840a0513
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-16 22:21:09 +0200

    Adding new endpoint getHashtagById

--- src/main/java/org/kryptokrona/hugin/service/HashtagService.java
+++ src/main/java/org/kryptokrona/hugin/service/HashtagService.java
@@ -1,7 +1,10 @@
 package org.kryptokrona.hugin.service;
 
+import org.kryptokrona.hugin.controller.HashtagController;
 import org.kryptokrona.hugin.model.Hashtag;
 import org.kryptokrona.hugin.repository.HashtagRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,10 +22,24 @@ public class HashtagService {
 	@Autowired
 	private HashtagRepository hashtagRepository;
 
+	private static final Logger logger = LoggerFactory.getLogger(HashtagService.class);
+
 	public Page<Hashtag> getAll(int page, int size) {
 		Page<Hashtag> pageTuts = null;
 		Pageable paging = PageRequest.of(page, size);
 
 		return hashtagRepository.findAll(paging);
 	}
-}
+
+	public Hashtag getById(long id) {
+		if (hashtagRepository.existsById(id)) {
+			Hashtag hashtag = hashtagRepository.findById(id).get();
+			logger.info("Hashtag found with ID: " + id);
+			return hashtag;
+		} else {
+			logger.error("Unable to find hashtag with ID: " + id);
+		}
+
+		return null;
+	}
+ }
