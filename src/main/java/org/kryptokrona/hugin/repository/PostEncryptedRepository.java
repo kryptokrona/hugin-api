88659d45abbd7905b8a6eef620d02ad3fbbe5fe1
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-16 21:14:57 +0200

    Adding checkers for entries in database

--- src/main/java/org/kryptokrona/hugin/repository/PostEncryptedRepository.java
+++ src/main/java/org/kryptokrona/hugin/repository/PostEncryptedRepository.java
@@ -11,4 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PostEncryptedRepository extends JpaRepository<PostEncrypted, Long> {
+	boolean existsPostByTxHash(String txHash);
 }
