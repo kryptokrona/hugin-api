83ba56af8322fe2d1d7e0af905a24de3c24dc4ce
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-16 21:23:18 +0200

    Adding skeleton methods in services

--- src/main/java/org/kryptokrona/hugin/service/PostService.java
+++ src/main/java/org/kryptokrona/hugin/service/PostService.java
@@ -16,24 +16,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostService {
 
-  @Autowired
-  private PostRepository postRepository;
+    @Autowired
+    private PostRepository postRepository;
 
-  public Page<Post> getAll(int page, int size) {
-    Page<Post> pageTuts = null;
-    Pageable paging = PageRequest.of(page, size);
+    public Page<Post> getAll(int page, int size) {
+        Page<Post> pageTuts = null;
+        Pageable paging = PageRequest.of(page, size);
 
-    return postRepository.findAll(paging);
-  }
+        return postRepository.findAll(paging);
+    }
 
-  /**
-   * Checks if the post exists in the database.
-   *
-   * @param txHash The unique transaction hash connected to the post object
-   * @return Returns if it exists or not
-   */
-  public boolean postExist(String txHash) {
-    return postRepository.existsPostByTxHash(txHash);
-  }
+    /**
+     * Checks if the post exists in the database.
+     *
+     * @param txHash The unique transaction hash connected to the post object
+     * @return Returns if it exists or not
+     */
+    public boolean postExist(String txHash) {
+        return postRepository.existsPostByTxHash(txHash);
+    }
+
+    public void savePost() {
+
+    }
 
 }
