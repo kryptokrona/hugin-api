fe1df7944f673e2020e631f7eaff70c9f5c568bf
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-21 00:16:57 +0200

    Updating configs

--- src/config/rateLimit.ts
+++ src/config/rateLimit.ts
@@ -4,14 +4,16 @@
 
 'use strict'
 
-require('dotenv').config()
-const rateLimit = require("express-rate-limit")
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
+
+import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: process.env.SYS_RATELIMIT_WINDOW_MS, // 15 minutes
-  max: process.env.SYS_RATELIMIT_MAX, // Limit each IP to 1000 requests per `window` (here, per 15 minutes) - TODO: perhaps put this in a settings.ini file?
+  windowMs: Number(process.env.SYS_RATELIMIT_WINDOW_MS), // 15 minutes
+  max: Number(process.env.SYS_RATELIMIT_MAX), // Limit each IP to 1000 requests per `window` (here, per 15 minutes) - TODO: perhaps put this in a settings.ini file?
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
-module.exports = { limiter }
+export default limiter;
