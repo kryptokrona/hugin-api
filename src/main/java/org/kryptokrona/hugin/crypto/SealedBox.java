1b4a7f83355230cba6d261831300705c191fefed
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-23 10:56:20 +0200

    Adding getters/setters to OpenBox and SealedBox

--- src/main/java/org/kryptokrona/hugin/crypto/SealedBox.java
+++ src/main/java/org/kryptokrona/hugin/crypto/SealedBox.java
@@ -5,4 +5,21 @@ public class SealedBox {
 	private String sb;
 
 	private long timestamp;
+
+	public String getSb() {
+		return sb;
+	}
+
+	public void setSb(String sb) {
+		this.sb = sb;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
 }
