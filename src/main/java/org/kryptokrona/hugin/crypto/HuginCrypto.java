52f790288237e6ff745cddfc3c02d9860fa98094
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-25 20:14:29 +0200

    Adding more logic to extraDataToPost method in HuginCrypto

--- src/main/java/org/kryptokrona/hugin/crypto/HuginCrypto.java
+++ src/main/java/org/kryptokrona/hugin/crypto/HuginCrypto.java
@@ -1,9 +1,12 @@
 package org.kryptokrona.hugin.crypto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.kryptokrona.hugin.model.Post;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.List;
@@ -87,16 +90,45 @@ public class HuginCrypto {
 	 * @param xkrKeyPair Regular Kryptokrona key pair
 	 * @return Returns open sealed box from the extra data
 	 */
-	public static Post extraDataToPost(String extra, List<String> knownKeys, KeyPair xkrKeyPair) {
+	public static Post extraDataToPost(String extra, List<String> knownKeys, KeyPair xkrKeyPair) throws IOException {
 		extra = trimExtra(extra);
 		var isBoxObj = HuginCrypto.isBoxObject(extra);
 		var isSealedBoxObj = HuginCrypto.isSealedBoxObject(extra);
 
+		var objectMapper = new ObjectMapper();
+
 		if (isBoxObj) {
+			var boxObj = objectMapper.readValue(new StringReader(extra), Box.class);
+			var i = 0;
+			var decryptBox = false;
+
+			try {
+
+			} catch (Exception e) {
+				logger.error("Could not decrypt box.");
+			}
+
+			while (i < knownKeys.size() && !decryptBox) {
+				var possibleKey = knownKeys.get(i);
+
+				i = i + 1;
+
+				try {
+					System.out.println("Remove this later.");
+				} catch (Exception e) {
+					logger.error("Could not decrypt with all known keys.");
+				}
+			}
+
+			if (!decryptBox) {
+				return null;
+			}
+
 			logger.info("box obj");
 		}
 
 		if (isSealedBoxObj) {
+			var boxObj = objectMapper.readValue(new StringReader(extra), SealedBox.class);
 			logger.info("sealed box obj");
 		}
 
