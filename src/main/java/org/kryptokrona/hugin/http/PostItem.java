d145255f14703dd49c139e82126171ca5ca212fa
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-25 20:41:01 +0200

    Adding more logic to HuginSyncer

---
+++ src/main/java/org/kryptokrona/hugin/http/PostItem.java
@@ -0,0 +1,75 @@
+package org.kryptokrona.hugin.http;
+
+public class PostItem {
+
+	private String m;
+
+	private String k;
+
+	private String s;
+
+	private String brd;
+
+	private long t;
+
+	private String n;
+
+	private String r;
+
+	public String getM() {
+		return m;
+	}
+
+	public void setM(String m) {
+		this.m = m;
+	}
+
+	public String getK() {
+		return k;
+	}
+
+	public void setK(String k) {
+		this.k = k;
+	}
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+	public String getBrd() {
+		return brd;
+	}
+
+	public void setBrd(String brd) {
+		this.brd = brd;
+	}
+
+	public long getT() {
+		return t;
+	}
+
+	public void setT(long t) {
+		this.t = t;
+	}
+
+	public String getN() {
+		return n;
+	}
+
+	public void setN(String n) {
+		this.n = n;
+	}
+
+	public String getR() {
+		return n;
+	}
+
+	public void setR(String r) {
+		this.r = r;
+	}
+
+}
