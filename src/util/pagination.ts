8ff6ba3d7d9c9935e6c4ab829ed67d102f7cf630
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-27 15:07:29 +0200

    Updating pagination

--- src/util/pagination.ts
+++ src/util/pagination.ts
@@ -15,7 +15,7 @@ async function getPagingData(data: any, page: number, limit: number) {
     const currentPage = page ? +page : 0;
     const totalPages = Math.ceil(totalItems / limit);
 
-    return { totalItems, items, totalPages, currentPage }
+    return { totalItems, items, totalPages, currentPage };
 }
 
 /**
