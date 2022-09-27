c11f87f8c640c234a3b150744944d600f629f4d8
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-09-27 15:08:49 +0200

    Updating server

--- src/server.ts
+++ src/server.ts
@@ -36,83 +36,83 @@ import limiter from "./config/rateLimit";
 
 var app = express()
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(__dirname + '/public'));
 
-app.locals.sitetitle = 'Hugin API'
+app.locals.sitetitle = 'Hugin API';
 
 // swagger
-const openapiSpecification = swaggerJsDoc(swaggerOptions)
+const openapiSpecification = swaggerJsDoc(swaggerOptions);
 
 // cache control middleware
-app.use(setCache)
+app.use(setCache);
 
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter);
 // if problem with reverse proxy try tweak this setting (https://www.npmjs.com/package/express-rate-limit)
-app.set('trust proxy', 1)
+app.set('trust proxy', 1);
 
 // api routes
-app.use(`${process.env.API_BASE_PATH}/docs`, swaggerUi.serve, swaggerUi.setup(openapiSpecification, swaggerCustomOptions))
+app.use(`${process.env.API_BASE_PATH}/docs`, swaggerUi.serve, swaggerUi.setup(openapiSpecification, swaggerCustomOptions));
 
 // latest routes
-app.use(`${process.env.API_BASE_PATH}/v1/`, postRouterLatest)
-app.use(`${process.env.API_BASE_PATH}/v1/`, postEncryptedRouterLatest)
-app.use(`${process.env.API_BASE_PATH}/v1/`, postEncryptedGroupRouterLatest)
-app.use(`${process.env.API_BASE_PATH}/v1/`, hashtagRouterLatest)
-app.use(`${process.env.API_BASE_PATH}/v1/`, statisticsRouterLatest)
+app.use(`${process.env.API_BASE_PATH}/v1/`, postRouterLatest);
+app.use(`${process.env.API_BASE_PATH}/v1/`, postEncryptedRouterLatest);
+app.use(`${process.env.API_BASE_PATH}/v1/`, postEncryptedGroupRouterLatest);
+app.use(`${process.env.API_BASE_PATH}/v1/`, hashtagRouterLatest);
+app.use(`${process.env.API_BASE_PATH}/v1/`, statisticsRouterLatest);
 
 app.use(bodyParser.json());
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
-    next(createError(404))
+    next(createError(404));
 })
 
 // error handler
 app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
     // 404 Not Found.
     if (err.status === 404) {
-        log.error(getTimestamp() + ' ERROR: 404 - Page could not be found.')
+        log.error(getTimestamp() + ' ERROR: 404 - Page could not be found.');
         return res
-            .status(404).json('ERROR: Not Found.')
+            .status(404).json('ERROR: Not Found.');
     }
 
     // 500 Internal Server Error (in production, all other errors send this response).
     if (req.app.get('env') !== 'development') {
-        log.error(getTimestamp() + ' ERROR: 500 - Internal server error.')
+        log.error(getTimestamp() + ' ERROR: 500 - Internal server error.');
         return res
             .status(500)
-            .json('ERROR: Not Found.')
+            .json('ERROR: Not Found.');
     }
 
     // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // send response back
-    res.status(err.status || 500)
+    res.status(err.status || 500);
 })
 
 // Start listening.
 app.listen(process.env.SYS_API_PORT, async () => {
-    console.log(`Server started on http://localhost:${process.env.SYS_API_PORT}`)
-    console.log('Press Ctrl-C to terminate...')
+    console.log(`Server started on http://localhost:${process.env.SYS_API_PORT}`);
+    console.log('Press Ctrl-C to terminate...');
 
     if (process.env.NODE_ENV === 'development') {
-        log.setLevel('trace')
+        log.setLevel('trace');
     }
 
     // do not start the hugin syncer if we want to test the endpoints
     if (process.env.NODE_ENV !== 'test') {
         // starting hugin sync
         while (true) {
-            await sleep(Number(process.env.SYS_HUGIN_SYNCER_SLEEP))
-            await backgroundSyncMessages()
+            await sleep(Number(process.env.SYS_HUGIN_SYNCER_SLEEP));
+            await backgroundSyncMessages();
         }
     }
 })
