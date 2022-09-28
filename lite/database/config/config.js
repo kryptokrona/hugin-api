dd66fb8d0234bfcf3fe382f394bd1e0375a9daaa
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-28 15:11:04 +0200

    Adding .dockerignore and .env

--- lite/database/config/config.js
+++ lite/database/config/config.js
@@ -5,10 +5,6 @@ module.exports = {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
   },
-  test: {
-    url: process.env.TEST_DATABASE_URL,
-    dialect: 'postgres',
-  },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
