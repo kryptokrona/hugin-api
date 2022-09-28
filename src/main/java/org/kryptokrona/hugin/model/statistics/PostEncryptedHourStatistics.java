bd68d0079c24eae12e64c0525c3955aac28973c8
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-29 01:04:18 +0200

    Adding new statistics entities

---
+++ src/main/java/org/kryptokrona/hugin/model/statistics/PostEncryptedHourStatistics.java
@@ -0,0 +1,51 @@
+package org.kryptokrona.hugin.model.statistics;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Post Hour Statistics.
+ *
+ * @author Marcus Cvjeticanin
+ */
+@Entity
+@Table(name = "statistics_post_hour")
+public class PostEncryptedHourStatistics {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private long amount;
+
+	@JsonProperty("created_at")
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private Date createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
