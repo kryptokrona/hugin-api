229867fec5372bd7e4ebf35fc3c2c1099f8e7798
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-28 23:20:40 +0200

    Updating statistics controller of hashtag

--- src/main/java/org/kryptokrona/hugin/controller/statistics/latest/StatisticsHashtagController.java
+++ src/main/java/org/kryptokrona/hugin/controller/statistics/latest/StatisticsHashtagController.java
@@ -1,9 +1,14 @@
 package org.kryptokrona.hugin.controller.statistics.latest;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.kryptokrona.hugin.http.StatisticsResponse;
 import org.kryptokrona.hugin.service.HashtagService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +32,50 @@ public class StatisticsHashtagController {
 	@Autowired
 	public StatisticsHashtagController(HashtagService hashtagService) {
 		this.hashtagService = hashtagService;
+	}
+
+	@GetMapping("/24h")
+	@Operation(
+			summary = "Amount of hashtags during last 24h",
+			description = "Gets the total amount of hashtags during last 24h."
+	)
+	public ResponseEntity<StatisticsResponse> getTotalAmount24h() {
+		return new ResponseEntity<>(
+				new StatisticsResponse(hashtagService.getTotalItemsBy24h()),
+				HttpStatus.OK);
+	}
+
+	@GetMapping("/week")
+	@Operation(
+			summary = "Amount of hashtags during last week",
+			description = "Gets the total amount of hashtags during last week."
+	)
+	public ResponseEntity<StatisticsResponse> getTotalAmountWeek() {
+		return new ResponseEntity<>(
+				new StatisticsResponse(hashtagService.getTotalItemsByWeek()),
+				HttpStatus.OK);
+	}
+
+	@GetMapping("/month")
+	@Operation(
+			summary = "Amount of posts during last month",
+			description = "Gets the total amount of hashtags during last month."
+	)
+	public ResponseEntity<StatisticsResponse> getTotalAmountMonth() {
+		return new ResponseEntity<>(
+				new StatisticsResponse(hashtagService.getTotalItemsByMonth()),
+				HttpStatus.OK);
+	}
+
+	@GetMapping("/year")
+	@Operation(
+			summary = "Amount of hashtags during last year",
+			description = "Gets the total amount of hashtags during last year."
+	)
+	public ResponseEntity<StatisticsResponse> getTotalAmountYear() {
+		return new ResponseEntity<>(
+				new StatisticsResponse(hashtagService.getTotalItemsByYear()),
+				HttpStatus.OK);
 	}
 
 }
