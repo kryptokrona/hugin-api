751f4a3efd9b7aae87c1b4188430fbe1ad4e063a
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-29 22:23:05 +0200

    Optimizing imports

--- src/main/java/org/kryptokrona/hugin/syncer/StatisticsSyncer.java
+++ src/main/java/org/kryptokrona/hugin/syncer/StatisticsSyncer.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Statistics Syncer.
  *
