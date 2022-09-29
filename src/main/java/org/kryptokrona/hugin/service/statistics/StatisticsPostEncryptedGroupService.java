0853ad511e457e7fb9f590f769f8ddc220627cd5
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-29 18:32:38 +0200

    Implementing StatisticsPostEncryptedService

--- src/main/java/org/kryptokrona/hugin/service/statistics/StatisticsPostEncryptedGroupService.java
+++ src/main/java/org/kryptokrona/hugin/service/statistics/StatisticsPostEncryptedGroupService.java
@@ -1,4 +1,24 @@
 package org.kryptokrona.hugin.service.statistics;
 
 public class StatisticsPostEncryptedGroupService {
+
+	public void save() {
+
+	}
+
+	public void save() {
+
+	}
+
+	public void save() {
+
+	}
+
+	public void save() {
+
+	}
+
+	public void save() {
+
+	}
 }
--- src/main/java/org/kryptokrona/hugin/service/statistics/StatisticsPostEncryptedService.java
+++ src/main/java/org/kryptokrona/hugin/service/statistics/StatisticsPostEncryptedService.java
@@ -1,7 +1,56 @@
 package org.kryptokrona.hugin.service.statistics;
 
+import org.kryptokrona.hugin.model.statistics.*;
+import org.kryptokrona.hugin.repository.statistics.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticsPostEncryptedService {
+
+	private final PostEncryptedHourStatisticsRepository postEncryptedHourStatisticsRepository;
+
+	private final PostEncrypted24hStatisticsRepository postEncrypted24hStatisticsRepository;
+
+	private final PostEncryptedWeekStatisticsRepository postEncryptedWeekStatisticsRepository;
+
+	private final PostEncryptedMonthStatisticsRepository postEncryptedMonthStatisticsRepository;
+
+	private final PostEncryptedYearStatisticsRepository postEncryptedYearStatisticsRepository;
+
+	@Autowired
+	public StatisticsPostEncryptedService(
+			PostEncryptedHourStatisticsRepository postEncryptedHourStatisticsRepository,
+			PostEncrypted24hStatisticsRepository postEncrypted24hStatisticsRepository,
+			PostEncryptedWeekStatisticsRepository postEncryptedWeekStatisticsRepository,
+			PostEncryptedMonthStatisticsRepository postEncryptedMonthStatisticsRepository,
+			PostEncryptedYearStatisticsRepository postEncryptedYearStatisticsRepository
+	) {
+		this.postEncryptedHourStatisticsRepository = postEncryptedHourStatisticsRepository;
+		this.postEncrypted24hStatisticsRepository = postEncrypted24hStatisticsRepository;
+		this.postEncryptedWeekStatisticsRepository = postEncryptedWeekStatisticsRepository;
+		this.postEncryptedMonthStatisticsRepository = postEncryptedMonthStatisticsRepository;
+		this.postEncryptedYearStatisticsRepository = postEncryptedYearStatisticsRepository;
+	}
+
+	public void save(PostEncryptedHourStatistics postEncryptedHourStatistics) {
+		postEncryptedHourStatisticsRepository.save(postEncryptedHourStatistics);
+	}
+
+	public void save(PostEncrypted24hStatistics postEncrypted24hStatistics) {
+		postEncrypted24hStatisticsRepository.save(postEncrypted24hStatistics);
+	}
+
+	public void save(PostEncryptedWeekStatistics postEncryptedWeekStatistics) {
+		postEncryptedWeekStatisticsRepository.save(postEncryptedWeekStatistics);
+	}
+
+	public void save(PostEncryptedMonthStatistics postEncryptedMonthStatistics) {
+		postEncryptedMonthStatisticsRepository.save(postEncryptedMonthStatistics);
+	}
+
+	public void save(PostEncryptedYearStatistics postEncryptedYearStatistics) {
+		postEncryptedYearStatisticsRepository.save(postEncryptedYearStatistics);
+	}
+
 }
