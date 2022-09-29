db1dfc557d1a4bb78a461cb65c62e4ec82ac61c5
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-09-29 18:04:03 +0200

    Constructor injection into StatisticsSyncer

--- src/main/java/org/kryptokrona/hugin/syncer/StatisticsSyncer.java
+++ src/main/java/org/kryptokrona/hugin/syncer/StatisticsSyncer.java
@@ -1,8 +1,10 @@
 package org.kryptokrona.hugin.syncer;
 
 import inet.ipaddr.HostName;
+import org.kryptokrona.hugin.service.statistics.StatisticsPostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class StatisticsSyncer {
 
+	private StatisticsPostService statisticsPostService;
+
 	private static final Logger logger = LoggerFactory.getLogger(StatisticsSyncer.class);
+
+	@Autowired
+	public StatisticsSyncer(StatisticsPostService statisticsPostService) {
+		this.statisticsPostService = statisticsPostService;
+	}
 
 	@Scheduled(fixedRate=1000)
 	public void sync() {
 		logger.debug("Statistics Syncing...");
-
+		// statisticsPostService.save();
 	}
 
 }
