cb0fdff99db0b80c038673b373330a4ec80fd36a
f-r00t <fruktstav@gmail.com>
2022-10-07 22:58:00 +0200

    Changed to encrypted wallet file

--- utils/wallet.js
+++ utils/wallet.js
@@ -12,16 +12,14 @@
  */
 module.exports.openWallet = async (daemon) => {
     console.log('Open wallet')
-    
+
     try {
         const json_wallet = await files.readFile("wallet.json");
-        let test = JSON.parse(json_wallet)
-        console.log('json wallet file?', JSON.parse(test))
-        const [wallet, err] = await WB.WalletBackend.loadWalletFromJSON(daemon, json_wallet);
-        console.log(err)
-        return wallet
+         const [wallet, error] = await WB.WalletBackend.openWalletFromEncryptedString(daemon, JSON.parse(json_wallet), 'hugin');
+         console.log('Wallet found, loading..');
+         return wallet
     } catch (err) {
-        console.log('Json wallet error, try backup wallet file', err)
+        console.log('No wallet found, creating a new one..', err)
         return await WB.WalletBackend.createWallet(daemon);
     }
 }
