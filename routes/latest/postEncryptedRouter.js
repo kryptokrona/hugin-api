d274268e11f5fea8c1601f7b2ce37156451f08bd
Marcus Cvjeticanin <mjovanc@icloud.com>
2022-10-07 23:52:58 +0200

    Fixing endpoints

--- routes/latest/postEncryptedRouter.js
+++ routes/latest/postEncryptedRouter.js
@@ -123,7 +123,7 @@ router.get('/posts-encrypted', controller.getAll)
  */
 router.get('/posts-encrypted/:tx_hash', controller.getEncryptedPostByTxHash)
 
-router.post('/posts-encrypted', controller.postMessage)
+router.post('/posts-encrypted', controller.sendMessage)
 
 
 module.exports = router
