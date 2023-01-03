56ab6a3ea46170ae25001d8f9942425325785c66
mjovanc <mjovanc@protonmail.com>
2023-01-03 19:28:59 +0100

    Adding more logic to start script

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 npm run db:migrate
-npm start
+
+# checking if we have set the alert environment variables or if they are empty strings
+if [[ ! -v DEPLOY_ENV ]]; then
+    echo "-- Using alert is OFF"
+    npm start
+elif [[ -z "$DEPLOY_ENV" ]]; then
+    echo "-- Using alert is OFF"
+    npm start
+else
+    echo "-- Using alert is ON"
+    npm start_alert
+fi
