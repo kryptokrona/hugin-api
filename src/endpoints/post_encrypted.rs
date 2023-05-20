e3c36b21865f7861c7a98f2fd0687632b9dd4682
mjovanc <mjovanc@icloud.com>
2023-05-20 13:19:45 +0200

    Updating endpoints

--- src/endpoints/post_encrypted.rs
+++ src/endpoints/post_encrypted.rs
@@ -1,16 +1,16 @@
 use rocket::serde::json::Json;
 
-#[post("/post-encrypted", format = "json")]
+#[get("/post-encrypted", format = "json")]
 pub fn get_all() -> Json<Vec<String>> {
     Json(vec!["Hello".to_string(), "World".to_string()])
 }
 
 #[post("/post-encrypted/<id>", format = "json")]
-pub fn get(id: usize) -> Json<String> {
-    Json(format!("Hello, {}!", id))
+pub fn get_by_id(id: usize) -> Json<String> {
+    Json(format!("Hello, {:?}!", id))
 }
 
-#[post("/post-encrypted/<tx_hash>", format = "json")]
-pub fn get(tx_hash: Json<String>) -> Json<String> {
-    Json(format!("Hello, {}!", tx_hash))
+#[post("/post-encrypted/<tx_hash>", format = "json", rank = 2)]
+pub fn get_by_tx_hash(tx_hash: &str) -> Json<String> {
+    Json(format!("Hello, {:?}!", tx_hash))
 }
--- src/endpoints/post_encrypted_group.rs
+++ src/endpoints/post_encrypted_group.rs
@@ -1,16 +1,16 @@
 use rocket::serde::json::Json;
 
-#[post("/post-encrypted-group", format = "json")]
+#[get("/post-encrypted-group", format = "json")]
 pub fn get_all() -> Json<Vec<String>> {
     Json(vec!["Hello".to_string(), "World".to_string()])
 }
 
 #[post("/post-encrypted-group/<id>", format = "json")]
-pub fn get(id: usize) -> Json<String> {
-    Json(format!("Hello, {}!", id))
+pub fn get_by_id(id: usize) -> Json<String> {
+    Json(format!("Hello, {:?}!", id))
 }
 
-#[post("/post-encrypted-group/<tx_hash>", format = "json")]
-pub fn get(tx_hash: Json<String>) -> Json<String> {
-    Json(format!("Hello, {}!", tx_hash))
+#[post("/post-encrypted-group/<tx_hash>", format = "json", rank = 2)]
+pub fn get_by_tx_hash(tx_hash: &str) -> Json<String> {
+    Json(format!("Hello, {:?}!", tx_hash))
 }
