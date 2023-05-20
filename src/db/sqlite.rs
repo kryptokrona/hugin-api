286ccaeb273a8d52e77a16a8b0093d1d58cd7bcf
mjovanc <mjovanc@icloud.com>
2023-05-20 18:13:10 +0200

    Adding init_db function in sqlite.rs

--- src/db/sqlite.rs
+++ src/db/sqlite.rs
@@ -1,1 +1,28 @@
+use sqlite::{Connection, State};
 
+fn init_db() -> Result<(), sqlite::Error> {
+    let conn = Connection::open("hugin_api.db")?;
+
+    conn.execute(
+        "
+        CREATE TABLE IF NOT EXISTS postencrypted (
+            tx_hash TEXT,
+            tx_box TEXT,
+            tx_timestamp INTEGER,
+            created_at DATE NOT NULL,
+            updated_at DATE NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS postencryptedgroup (
+            tx_hash TEXT,
+            tx_sb TEXT,
+            tx_timestamp INTEGER,
+            created_at DATE NOT NULL,
+            updated_at DATE NOT NULL
+        );
+
+        ",
+    )?;
+
+    Ok(())
+}
