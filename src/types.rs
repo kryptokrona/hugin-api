e5a1b706bff05233714af121163b440427481411
mjovanc <mjovanc@icloud.com>
2023-05-20 17:47:42 +0200

    Adding PoolChangeLiteResponse struct

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,14 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct PoolChangesLiteRequest<'r> {
     pub known_txs_ids: &'r Vec<&'r str>,
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PoolChangesLiteResponse {
+    pub added_txs: Vec<String>,
+    pub removed_txs: Vec<String>,
 }
